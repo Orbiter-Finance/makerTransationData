9721ba1f46ce877341dede7839a4a337a3253d46
0xKate <huanhui.boy@gmail.com>
2022-08-02 17:01:18 +0800

    fix calc transfer amount

--- src/utils/oldUtils.ts
+++ src/utils/oldUtils.ts
@@ -172,11 +172,10 @@ const performUserAmountLegality = (userAmount: BigNumber, chain: any) => {
   return new BigNumber(convertResult);
 };
 function getToAmountFromUserAmount(
-  userAmount: BigNumber,
+  userAmount: any,
   selectMakerInfo: any,
   isWei: any,
 ) {
-  userAmount = performUserAmountLegality(userAmount, selectMakerInfo);
   let toAmount_tradingFee = new BigNumber(userAmount).minus(
     new BigNumber(selectMakerInfo.tradingFee),
   );
@@ -184,10 +183,12 @@ function getToAmountFromUserAmount(
     .multipliedBy(new BigNumber(selectMakerInfo.gasFee))
     .dividedBy(new BigNumber(1000));
   let digit = selectMakerInfo.precision === 18 ? 5 : 2;
+  // accessLogger.info('digit =', digit)
   let gasFee_fix = gasFee.decimalPlaces(digit, BigNumber.ROUND_UP);
+  // accessLogger.info('gasFee_fix =', gasFee_fix.toString())
   let toAmount_fee = toAmount_tradingFee.minus(gasFee_fix);
-
-  if (!toAmount_fee || isNaN(toAmount_fee.toNumber())) {
+  // accessLogger.info('toAmount_fee =', toAmount_fee.toString())
+  if (!toAmount_fee || isNaN(Number(toAmount_fee))) {
     return 0;
   }
   if (isWei) {

2d23d51923ffcc7dcab832adfd305ebd64d84568
rldwzl <719779468@qq.com>
2022-12-03 12:26:16 +0800

    xvm data

--- src/service/transaction.ts
+++ src/service/transaction.ts
@@ -206,6 +206,10 @@ export async function bulkCreateTransaction(
     const saveExtra: any = {
       ebcId: "",
     };
+    if (tx.source == "xvm") {
+      Object.assign(saveExtra, txExtra.xvm || {});
+      console.log(txExtra.xvm?.isOk ? "xvm swapOk" : "xvm swapFail");
+    }
     const isMakerSend =
       ctx.makerConfigs.findIndex((row: { sender: any }) =>
         equals(row.sender, tx.from),

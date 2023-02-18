18000a65532baf6785d3e601ddbe3f84919b1756
kakui <huanhui.boy@gmail.com>
2023-02-18 12:20:25 +0800

    change logger

--- src/service/watch.ts
+++ src/service/watch.ts
@@ -129,7 +129,10 @@ export class Watch {
   }
   // read db
   public async readUserSendReMatch(): Promise<any> {
-    const startAt = dayjs().subtract(60 * 6, "minute").startOf("d").toDate();
+    const startAt = dayjs()
+      .subtract(60 * 6, "minute")
+      .startOf("d")
+      .toDate();
     let endAt = dayjs().subtract(1, "minute").toDate();
     const where = {
       side: 0,
@@ -157,7 +160,11 @@ export class Watch {
             error,
           );
         });
-        console.log('processUserSendMakerTx:', tx.hash, `max:${txList.length},current:${i}`);
+        console.log(
+          "processUserSendMakerTx:",
+          tx.hash,
+          `max:${txList.length},current:${i}`,
+        );
         i++;
         endAt = tx.timestamp;
       }

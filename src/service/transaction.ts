440ad34395ca849de5786b97e64331ee5e8a29bd
skywell <719779468@qq.com>
2023-02-21 14:13:36 +0800

    status : 97

--- src/service/transaction.ts
+++ src/service/transaction.ts
@@ -711,7 +711,9 @@ export async function processMakerSendUserTx(
     const models = ctx.models;
     const where: any = {
       transferId: makerTx.transferId,
-      status: 1,
+      status: {
+        [Op.or]: [1, 97],
+      },
       side: 0,
       timestamp: {
         [Op.lte]: dayjs(makerTx.timestamp).add(30, "m").toDate(),

ada2f9308341da18015e5b24a7c8a90da2a4e4f9
skywell <719779468@qq.com>
2023-03-08 14:28:36 +0800

    orbiter-chaincore version

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 // export const isLocal = () => process.env.NODE_ENV === 'test';
 // export const isDev = () => process.env.NODE_ENV === 'development';
-export const isProd = () => process.env.NODE_ENV === 'production';
+export const isProd = () => process.env.NODE_ENV === "production";
 
 export default {
   development: {

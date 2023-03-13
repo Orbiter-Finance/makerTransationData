2f298848dd1d1986c8c9d162d72f1c28bcd79a73
skywell <719779468@qq.com>
2023-03-13 18:44:01 +0800

    boot time

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,6 @@
 import { Context } from "./context";
 import Koa2 from "koa";
 import router from "./server/router";
-import fs from "fs";
-import path from "path";
-import { getFormatDate } from "./utils/oldUtils";
 export function createServer(spvCtx: Context) {
   const app = new Koa2();
   app.use(async (ctx, next) => {
@@ -22,12 +19,6 @@ export function createServer(spvCtx: Context) {
   const port = process.env["PORT"] || 3000;
   app.listen(port, () => {
     console.log(`Api Service Start: http://127.0.0.1:${port}`);
-    fs.writeFileSync(
-      path.join(__dirname, "../../bootInfo.txt"),
-      `Time: ${getFormatDate(
-        new Date().valueOf(),
-      )}\nService: http://127.0.0.1:${port}`,
-    );
   });
   return app;
 }

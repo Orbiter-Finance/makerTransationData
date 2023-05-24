9764abc8880974cff86b0b3d34ff538268a02c68
skywell <719779468@qq.com>
2023-05-24 12:07:16 +0800

    check account address

--- src/service/transaction.ts
+++ src/service/transaction.ts
@@ -14,6 +14,7 @@ import { InferAttributes, InferCreationAttributes, Op } from "sequelize";
 import { Context } from "../context";
 import { TranferId, TransactionID } from "../utils";
 import {
+  getAccountAddressError,
   getAmountFlag,
   getAmountToSend,
   getFormatDate,
@@ -269,7 +270,12 @@ export async function bulkCreateTransaction(
           true,
           String(txData.to)
         );
-        if (!market || isEmpty(txData.replyAccount)) {
+
+        const error: string | null = getAccountAddressError(txData.replyAccount, toChainId);
+        if (error) {
+          ctx.logger.error(`Illegal user starknet address ${txData.replyAccount} hash:${txData.hash}, ${error}`);
+        }
+        if (!market || isEmpty(txData.replyAccount) || error) {
           // market not found
           txData.status = 3;
         } else {
